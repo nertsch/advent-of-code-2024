d93e7782913bfa53bc01df040a68eb7dc0238891
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2024-12-09 00:30:01 +0100

    Day07b

--- src/day_07.rs
+++ src/day_07.rs
@@ -1,4 +1,11 @@
 pub fn part_a() -> u64{
+    get_calibration_result(2)
+}
+pub fn part_b() -> u64{
+    get_calibration_result(3)
+}
+
+pub fn get_calibration_result(number_of_operators : u32) -> u64{
     let my_str = include_str!("inputs/input07.txt");
     let mut sum = 0;
 
@@ -10,8 +17,8 @@ pub fn part_a() -> u64{
                 .map(|i| i.parse::<u64>().unwrap())
                 .collect();
 
-            for operator_instruction in 0..=(1 << inputs.len()-1) {
-                    if evaluate(operator_instruction, &inputs) == test_value {
+            for operator_instruction in 0..=(number_of_operators.pow((inputs.len() - 1) as u32)) {
+                    if evaluate(operator_instruction, &inputs, number_of_operators) == test_value {
                         sum += test_value;
                         break;
                     }
@@ -24,7 +31,7 @@ pub fn part_a() -> u64{
     sum
 }
 
-pub fn evaluate(mut operator_instruction: u32, inputs : &[u64]) -> u64 {
+pub fn evaluate(mut operator_instruction: u32, inputs : &[u64], number_of_operators: u32) -> u64 {
 
     if !matches!(inputs.len(), 2..=32) {
         panic!("inputs must be greater or equal than 2 and smaller or equal than 32")
@@ -36,8 +43,22 @@ pub fn evaluate(mut operator_instruction: u32, inputs : &[u64]) -> u64 {
         .fold(
             inputs[0],
              |acc, input| {
-                 let res = if operator_instruction & 1 == 0 { acc+input } else { acc*input};
-                 operator_instruction >>= 1;
+                 let operator_index = operator_instruction % number_of_operators;
+                 let res = match operator_index {
+                     0 => acc+input,
+                     1 => acc*input,
+                     _ => {
+                         let mut curr_input = *input;
+                         let mut factor = 1;
+                         while curr_input > 0 {
+                             curr_input /= 10;
+                             factor*=10;
+                         }
+                         acc * factor + input
+                     }
+                 };
+
+                 operator_instruction /= number_of_operators;
                  res
              })
 }
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,5 @@ mod day_07;
 fn main() {
     println!("{}", day_01::part_a());
     println!("{}", day_07::part_a());
+    println!("{}", day_07::part_b());
 }
