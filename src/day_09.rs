8ff42060e2368b7058bc1ae63b22a95d967934e4
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2024-12-09 21:25:11 +0100

    Day 9b

--- src/day_09.rs
+++ src/day_09.rs
@@ -15,6 +15,38 @@ pub fn part_a() -> u64 {
     disk_contents.iter().enumerate().map(|(i,id)| (i as u64)*id.unwrap_or(0)).sum()
 }
 
+pub fn part_b() -> u64 {
+    let mut disk_contents = read_disk_contents();
+
+    let mut tail = disk_contents.len()-1;
+    'main_loop: loop {
+        while disk_contents[tail].is_none() {tail-=1;}
+        let file_end = tail;
+        let file_id = disk_contents[tail].unwrap();
+        while disk_contents[tail] == Some(file_id) {
+            if tail == 0 {break 'main_loop;}
+            tail-=1;
+        }
+        let file_range = tail+1..file_end+1;
+
+        let mut free_block_start = 0usize;
+        loop {
+            while disk_contents[free_block_start].is_some() {free_block_start+=1;}
+            if(free_block_start > file_range.start) {break}
+            let mut free_block_end = free_block_start;
+            while disk_contents[free_block_end].is_none() {free_block_end+=1;}
+            if free_block_end-free_block_start >= file_range.len() {
+                let (left, right) = disk_contents.split_at_mut(file_range.start);
+                left[free_block_start..free_block_start+file_range.len()].swap_with_slice(&mut right[0..file_range.len()]);
+                break;
+            }
+            free_block_start = free_block_end;
+        }
+    }
+
+    disk_contents.iter().enumerate().map(|(i,id)| (i as u64)*id.unwrap_or(0)).sum()
+}
+
 fn read_disk_contents() -> Vec<Option<u64>>{
     let input = include_str!("inputs/input09.txt");
     let mut blocks: Vec<Option<u64>> = Vec::new();
