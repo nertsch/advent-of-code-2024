d3c6cfa7ee075bc2eaaac5716213abe408fdefaf
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2024-12-10 00:26:54 +0100

    Improve performance by justt using lists in part b

--- src/day_09.rs
+++ src/day_09.rs
@@ -2,7 +2,14 @@ use std::iter;
 use std::ops::Range;
 
 pub fn part_a() -> u64 {
-    let mut disk_contents = read_disk_contents();
+    let input = include_str!("inputs/input09.txt");
+    let mut disk_contents: Vec<Option<u64>> = Vec::new();
+
+    for (i, space_size) in input.chars().enumerate()  {
+        let i = i as u64;
+        let space_size = space_size.to_digit(10).expect("input must just contain numbers") as usize;
+        disk_contents.extend(iter::repeat(if i%2 == 0 {Some(i/2)} else { None }).take(space_size))
+    }
 
     let mut head = 0usize;
     let mut tail = disk_contents.len()-1;
@@ -18,51 +25,33 @@ pub fn part_a() -> u64 {
 }
 
 pub fn part_b() -> u64 {
-    let mut disk_contents = read_disk_contents();
+    let input = include_str!("inputs/input09.txt");
 
-    let mut prev_block_contents = disk_contents[0];
-    let mut last_delta_index = 0;
-    let mut file_list: Vec<Range<usize>> = Vec::new();
+    let mut file_list: Vec<(Range<usize>, u64)> = Vec::new();
     let mut free_list: Vec<Range<usize>> = Vec::new();
+    let mut current_index = 0;
 
-    for (index,block_contents) in disk_contents.iter().enumerate().skip(1){
-        match (prev_block_contents, block_contents ){
-            (None, Some(id)) => { free_list.push(last_delta_index..index); last_delta_index = index ; prev_block_contents = *block_contents;  }
-            (Some(_), None) => { file_list.push(last_delta_index..index); last_delta_index = index; prev_block_contents = *block_contents; }
-            (Some(a), Some(b)) if a != *b => { file_list.push(last_delta_index..index); last_delta_index = index; prev_block_contents = *block_contents; },
-            _ => {}
+    for (i, space_size) in input.chars().enumerate() {
+        let space_size = space_size.to_digit(10).expect("input must just contain numbers") as usize;
+        let space_range = current_index..current_index + space_size;
+        current_index = space_range.end;
+        if (i % 2 == 0) {
+            file_list.push((space_range, i as u64 / 2));
+        } else {
+            free_list.push(space_range);
         }
     }
 
-    match(prev_block_contents){
-        (Some(_)) => file_list.push(last_delta_index..disk_contents.len()),
-        None => free_list.push(last_delta_index..disk_contents.len()),
-    }
-
-
-    for file in file_list.iter().rev()  {
-        for free in free_list.iter_mut().filter(|f| f.end <= file.start){
-            if(free.len() >= file.len()) {
-                let (left, right) = disk_contents.split_at_mut(file.start);
-                left[free.start..free.start+file.len()].swap_with_slice(&mut right[0..file.len()]);
-                *free = free.start+file.len()..free.end;
+    for file in file_list.iter_mut().rev() {
+        for free in free_list.iter_mut().filter(|f| f.end <= file.0.start) {
+            if (free.len() >= file.0.len()) {
+                let free_start = free.start;
+                *free = free_start + file.0.len()..free.end;
+                *file = (free_start..free_start + file.0.len(), file.1);
                 break;
             }
         }
     }
 
-    disk_contents.iter().enumerate().map(|(i,id)| (i as u64)*id.unwrap_or(0)).sum()
-}
-
-fn read_disk_contents() -> Vec<Option<u64>>{
-    let input = include_str!("inputs/input09.txt");
-    let mut blocks: Vec<Option<u64>> = Vec::new();
-
-    for (i, space_size) in input.chars().enumerate()  {
-        let i = i as u64;
-        let space_size = space_size.to_digit(10).expect("input must just contain numbers") as usize;
-        blocks.extend(iter::repeat(if i%2 == 0 {Some(i/2)} else { None }).take(space_size))
-    }
-
-    blocks
+    file_list.into_iter().map(|f| f.0.sum::<usize>() as u64 * f.1).sum()
 }
