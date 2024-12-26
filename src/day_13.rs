b630aa013a30e4de0cc013e604c670c12a19cc16
Gerhard Zehetbauer <gerhard.zehetbauer@chello.at>
2024-12-26 23:52:17 +0100

    day_13

--- src/day_13.rs
+++ src/day_13.rs
@@ -1,61 +1,45 @@
-use nalgebra::Vector2;
+use regex::Regex;
 
 pub fn part_a() -> i32 {
     let machine_configurations = read_machine_configurations();
+
+    for c in machine_configurations.iter() {
+        let num = c.prize.1*c.button_a.0 - c.prize.0*c.button_a.1;
+        let den = c.button_b.1*c.button_a.0 - c.button_b.0*c.button_a.1;
+        if(num % den == 0) {
+            let b = num / den;
+
+        }
+    }
+
     0
 }
 
 struct MachineConfiguration {
-    distance_a: Vector2<i32>,
-    distance_b: Vector2<i32>,
-    price_location: Vector2<i32>,
+    button_a: (u32, u32),
+    button_b: (u32, u32),
+    prize: (u32, u32),
 }
 
 fn read_machine_configurations() -> Vec<MachineConfiguration> {
-    let mut input_lines = include_str!("inputs/input13.txt").lines();
-    let mut machine_configurations = Vec::<_>::new();
+    let input = include_str!("inputs/input13.txt");
 
-    while {
-        let distance_a = read_distance("Button A", '+', input_lines.next().unwrap());
-        let distance_b = read_distance("Button B", '+', input_lines.next().unwrap());
-        let price_location = read_distance("Prize", '=', input_lines.next().unwrap());
+    let machine_regex = Regex::new(r"(?s)Button A: X\+(?<ax>\d+), Y\+(?<ay>\d+)\s+Button B: X\+(?<bx>\d+), Y\+(?<by>\d+)\s+Prize: X=(?<px>\d+), Y=(?<py>\d+)\s*").unwrap();
 
-        machine_configurations.push(MachineConfiguration {
-            distance_a,
-            distance_b,
-            price_location,
-        });
+    let mut result = Vec::<_>::new();
 
-        input_lines.next().is_some()
-    } {}
+    for capture in machine_regex.captures_iter(input) {
+        println!("{:?}", capture.get(0).unwrap());
+        result.push(
+            MachineConfiguration{
+                button_a: (capture.name("ax").unwrap().as_str().parse().unwrap(),capture.name("ay").unwrap().as_str().parse().unwrap()),
+                button_b: (capture.name("bx").unwrap().as_str().parse().unwrap(),capture.name("by").unwrap().as_str().parse().unwrap()),
+                prize: (capture.name("px").unwrap().as_str().parse().unwrap(),capture.name("py").unwrap().as_str().parse().unwrap()),
+            }
+        );
+    }
 
-    machine_configurations
-}
-
-fn read_distance(expected_prelude: &str, dimension_delimiter: char, line: &str) -> Vector2<i32> {
-    line.split_once(':')
-        .filter(|(prelude, _)| *prelude == expected_prelude)
-        .map(|(_, v)| v)
-        .map(str::trim)
-        .map(|s| s.split_once(','))
-        .flatten()
-        .map(|(x, y)| {
-            (
-                read_dimension("X", dimension_delimiter, str::trim(x)),
-                read_dimension("Y", dimension_delimiter, str::trim(y)),
-            )
-        })
-        .map(|(x, y)| Vector2::<_>::new(x, y))
-        .expect("invalid line")
-}
-
-fn read_dimension(expected_prelude: &str, delimiter: char, input: &str) -> i32 {
-    input
-        .split_once(delimiter)
-        .filter(|(prelude, value)| *prelude == expected_prelude)
-        .map(|(_, value)| value.parse().ok())
-        .flatten()
-        .expect(&format!("invalid dimension {}", input))
+    result
 }
 
 pub fn part_b() -> i32 {
